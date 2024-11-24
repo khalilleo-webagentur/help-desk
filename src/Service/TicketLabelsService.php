3423f3b827672465caf55e7cb0260c9f144d1fad
khalilleo <dev@khalilleo.com>
2024-11-24 02:28:07 +0100

    crud ticket-labels

--- src/Service/TicketLabelsService.php
+++ src/Service/TicketLabelsService.php
@@ -12,7 +12,8 @@ final readonly class TicketLabelsService
 {
     public function __construct(
         private TicketLabelRepository $ticketLabelRepository,
-    ) {
+    )
+    {
     }
 
     public function getById(int $id): ?TicketLabel
@@ -20,10 +21,28 @@ final readonly class TicketLabelsService
         return $this->ticketLabelRepository->find($id);
     }
 
+    public function getOneByName(string $name): ?TicketLabel
+    {
+        return $this->ticketLabelRepository->findOneBy(['name' => $name]);
+    }
+
+    /**
+     * @return TicketLabel[]
+     */
+    public function getAll(): array
+    {
+        return $this->ticketLabelRepository->findAll();
+    }
+
     public function save(TicketLabel $model): TicketLabel
     {
         $this->ticketLabelRepository->save($model->setUpdatedAt(new DateTime()), true);
 
         return $model;
+    }
+
+    public function delete(TicketLabel $model): void
+    {
+        $this->ticketLabelRepository->remove($model, true);
     }
 }
