2b86cb1d9aec7ce628c85fee100523d0116f4053
khalilleo <dev@khalilleo.com>
2024-11-26 18:25:53 +0100

    dashboard home

--- src/Controller/Dashboard/IndexController.php
+++ src/Controller/Dashboard/IndexController.php
@@ -4,17 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller\Dashboard;
 
+use App\Service\UserService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('dashboard/o3o4v1v3a1g8h2q2')]
 class IndexController extends AbstractDashboardController
 {
+    public function __construct(
+        private readonly UserService $userService,
+    ){
+    }
+
     #[Route('/home', name: 'app_dashboard_index')]
     public function index(): Response
     {
         $this->denyAccessUnlessGrantedRoleCustomer();
 
-        return $this->render('dashboard/index.html.twig');
+        $user = $this->getUser();
+
+        $isAdmin = $this->userService->isAdmin($user);
+
+        $countIssues = $isAdmin ? 124 : 9;
+        $countClosedIssues = $isAdmin ? 124 : 9;
+        $countIssuesInProgress = $isAdmin ? 124 : 9;
+        $countOpenIssues = $isAdmin ? 124 : 9;
+
+        return $this->render('dashboard/index.html.twig', [
+            'countIssues' => $countIssues,
+            'countClosedIssues' => $countClosedIssues,
+            'countIssuesInProgress' => $countIssuesInProgress,
+            'countOpenIssues' => $countOpenIssues,
+        ]);
     }
 }
