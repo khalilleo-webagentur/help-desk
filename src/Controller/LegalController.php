038a5a0cc70e5bad2b810bbb7a24706be68ca979
khalilleo <dev@khalilleo.com>
2025-06-29 00:59:04 +0200

    legal imprint controller

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -16,6 +16,29 @@ class LegalController extends AbstractController
     ) {
     }
 
+    #[Route('/imprint/i7l0qxk9e7o1z3r9', name: 'app_legal_imprint')]
+    public function imprint(): Response
+    {
+        $generalManager = $this->configService->getParameter('generalManager');
+        $taxNumber = $this->configService->getParameter('taxNumber');
+        $street = $this->configService->getParameter('addressStreet');
+        $plz = $this->configService->getParameter('addressPlz');
+        $city = $this->configService->getParameter('addressCity');
+        $country = $this->configService->getParameter('addressCountry');
+        $phoneNumber = $this->configService->getParameter('phoneNumber');
+        $email = $this->configService->getParameter('legalEmail');
+        $poweredBy = $this->configService->getParameter('app_made_by');
+
+        return $this->render('static/legal/imprint.html.twig', [
+            'generalManager' => $generalManager,
+            'taxNumber' => $taxNumber,
+            'address' => $street . ', ' . $plz . ' ' . $city . ' ' . $country,
+            'phoneNumber' => $phoneNumber,
+            'email' => $email,
+            'poweredBy' => $poweredBy,
+        ]);
+    }
+
     #[Route('/privacy-policy/y7l0q6k9e1o1z3r5', name: 'app_legal_privacy_policy')]
     public function privacyPolicy(): Response
     {
@@ -26,7 +49,7 @@ class LegalController extends AbstractController
         $phoneNumber = $this->configService->getParameter('phoneNumber');
         $email = $this->configService->getParameter('legalEmail');
 
-        return $this->render('static/privacy-policy.html.twig', [
+        return $this->render('static/legal/privacy-policy.html.twig', [
             'street' => $street,
             'plz' => $plz,
             'city' => $city,
